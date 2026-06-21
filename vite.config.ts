b9bf09668a9d9e0e596340739eda5c2468878865
Chris Joe <flamer.ohr@gmail.com>
2026-06-21 23:03:41 +1200

    docs: build and publish

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import babel from "@rolldown/plugin-babel";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/pebble-scifi-neon-config/",
   build: {
     outDir: "docs",
   },
